33cdd809d09e104b996c3fbfce9feb58fe0737c5
ife <ife>
2025-01-23 04:00:14 -0800

    Registration Screen

---
+++ android/app/src/main/kotlin/com/example/registration_/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.registration_
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Screens/Registration_Screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final TextEditingController firstnameController = TextEditingController();
+  final TextEditingController lastnameContoller = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  bool isRegistered = false;
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Registration Form"),
+    ),
+  );
+}
