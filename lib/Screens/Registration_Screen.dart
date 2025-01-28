1768a91ab682ce74389a2629b9aff001a780a454
ife <ife>
2025-01-28 04:54:43 -0800

    Registration_Screen

--- lib/Screens/Registration_Screen.dart
+++ lib/Screens/Registration_Screen.dart
@@ -5,17 +5,65 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-  final TextEditingController firstnameController = TextEditingController();
-  final TextEditingController lastnameContoller = TextEditingController();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   bool isRegistered = false;
-}
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Registration Form"),
-    ),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registration Form"),
+      ), //AppBar
+
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: firstNameController,
+              decoration: const InputDecoration(labelText: "First Name"),
+            ),
+            TextField(
+                controller: lastNameController,
+                decoration: const InputDecoration(labelText: "Last Name")),
+            TextField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: "Email Address")),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isRegistered = !isRegistered;
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: isRegistered ? Colors.green: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+
+                  
+
+                
+                ),
+                child: Text(
+                  isRegistered?"Registration Successful":"Register me",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  )
+                   ),
+                ), 
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
